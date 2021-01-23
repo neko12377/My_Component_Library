0d12b275bb78c7dda0d009343a4f7c9548d75c19
Kasper Chen <neko12377@gmail.com>
2021-01-23 08:53:28 +0800

    feat: template Beta2

--- src/oneFile/Template.tsx
+++ src/oneFile/Template.tsx
@@ -122,15 +122,44 @@ export const App = () => {
                 <div className="main-content-tradeBar"></div>
                 <div className="main-content-canvas"></div>
                 <div className="main-content-tradeInfo">
-                    <div className="main-content-tradeInfo-title"></div>
-                    <div className="main-content-tradeInfo-investAmountBar"></div>
-                    <div className="main-content-tradeInfo-investAmountOption"></div>
-                    <div className="main-content-tradeInfo-utility">
-                        <div className="main-content-tradeInfo-utility-highLowButton"></div>
-                        <div className="main-content-tradeInfo-utility-investButton"></div>
+                    <div className="main-content-tradeInfo-title">
+                        GOLD
                     </div>
-                    <div className="main-content-tradeInfo-payout"></div>
-                    <div className="main-content-tradeInfo-return"></div>
+                    <div className="main-content-tradeInfo-investAmountBar">
+                        $500
+                    </div>
+                    <div className="main-content-tradeInfo-investAmountOption">
+                        <div className="main-content-tradeInfo-investAmountOption-Button">
+                            $50
+                        </div>
+                        <div className="main-content-tradeInfo-investAmountOption-Button">
+                            $100
+                        </div>
+                        <div className="main-content-tradeInfo-investAmountOption-Button-Focused">
+                            $500
+                        </div>
+                    </div>
+                    <div className="main-content-tradeInfo-utility">
+                        <div className="main-content-tradeInfo-utility-highLowButton">
+                            <div className="main-content-tradeInfo-utility-highLowButton-high">
+                                HIGH
+                            </div>
+                            <div className="main-content-tradeInfo-utility-highLowButton-low">
+                                LOW
+                            </div>
+                        </div>
+                        <div className="main-content-tradeInfo-utility-investButton">
+                            Invest
+                        </div>
+                    </div>
+                    <div className="main-content-tradeInfo-payout">
+                        <div>Payout</div>
+                        <div>$1.85</div>
+                    </div>
+                    <div className="main-content-tradeInfo-return">
+                        <div>Return</div>
+                        <div>$925.00</div>
+                    </div>
                 </div>
             </section>
             <section className="currentActivity-header">
